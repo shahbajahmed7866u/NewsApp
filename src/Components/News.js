a19a155cb486d0501d0dc6ad95d86eca1720988f
shahbajahmed7866u <shahbajahmed7866u@gmail.com>
2023-06-01 02:33:13 +0530

    made a page tab from which you can switch pages make code little bit clean as well

--- src/Components/News.js
+++ src/Components/News.js
@@ -69,36 +69,73 @@ constructor(){
   console.log("Hello")
   this.state={
 articles:this.articles,
-loading:false
+loading:false,
+page:1
   }
 }
 
 async componentDidMount(){
 console.log("cmp")
-let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=f9db77eef4174b9b8af7ca428ca9c97f"
+let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f9db77eef4174b9b8af7ca428ca9c97f&pageSize=${this.props.pageSize}&page=1`
 
 let data = await fetch(url);
 let paredData = await data.json()
 console.log(paredData)
-this.setState({articles:paredData.articles})
+this.setState({articles:paredData.articles, totalResults: paredData.totalResults})
 }
+hanlePrevClick = async () =>{
+  console.log("prev")
+  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f9db77eef4174b9b8af7ca428ca9c97f&pageSize=${this.props.pageSize}&page=${this.state.page-1}`
+
+  let data = await fetch(url);
+  let paredData = await data.json()
+  console.log(paredData)
+    this.setState({
+   page:this.state.page -1,
+   articles:paredData.articles
+    })
+}
+hanleNextClick = async () =>{
+  if(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)){
+    
+  }
+  else{
+    console.log("next")
+    console.log(this.state.page)
+    console.log(this.state.totalResults)
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f9db77eef4174b9b8af7ca428ca9c97f&pageSize=${this.props.pageSize}&page=${this.state.page+1}`
+    
+    let data = await fetch(url);
+    let paredData = await data.json()
+    console.log(paredData)
+    this.setState({
+      page:this.state.page +1,
+      articles:paredData.articles
+    })
+  }
+}
+
 
   render() {
     console.log("render")
     return (
       <div className='container my-3'>
-        <h2>NewsPedia - Top Headlines</h2>
+        <h1 className='text-center'>NewsPedia - Top Headlines</h1>
         <div className="row">
         {this.state.articles.map((elem)=>{
-          return  <div className="col-md-4" key={elem.url}>
+          return  <div className="col-md-4 d-flex justify-content-center" key={elem.url}>
             
           <Newsitem  title={elem.title?elem.title:""} description={elem.description?elem.description:""} imageUrl={elem.urlToImage} newsUrl={elem.url}/>
               </div>
-        })}
-          
-         
+        })} 
         </div>
-        
+        <div className="container d-flex justify-content-center " >
+        <div className="btn-group" role="group" aria-label="Basic outlined example">
+  <button disabled={this.state.page<=1}type="button" className="btn btn-outline-primary" onClick={this.hanlePrevClick}>&larr; Previous</button>
+  <button disabled={this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-outline-primary" onClick={this.hanleNextClick}>Next &rarr;</button>
+</div>
+        </div>
       </div>
     )
   }
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -4,13 +4,17 @@ export class Newsitem extends Component {
   render() {
     let { title, description, imageUrl, newsUrl } = this.props
     return (
-      <div className='my-3'>
-        <div className="card" style={{ width: "18rem" }}>
+      <div className='m-4 my-3 d-flex'>
+        <div className="card">
           <img src={imageUrl?imageUrl:"https://www.hindustantimes.com/ht-img/img/2023/05/28/1600x900/Prime-Minister-Narendra-Modi--File-Photo-_1685100967303_1685232074082.jpg"} className="card-img-top" alt="..." />
-          <div className="card-body">
+          <div className="card-body d-flex flex-column justify-content-between align-items-center p-3">
+            <div>
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+            </div>
+            <div>
+            <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+            </div>
           </div>
         </div>
       </div>
