ea88502baf36b25030bde7d3ad8ebeaa1076fd69
shahbajahmed7866u <shahbajahmed7866u@gmail.com>
2023-06-02 05:30:27 +0530

    added some more functionality to it cleaned the code as well

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,58 +3,41 @@ import Newsitem from './Newsitem'
 import Spinner from './Spinner'
 export class News extends Component {
   articles = []
-constructor(){
-  super();
-  console.log("Hello")
-  this.state={
-articles:this.articles,
-loading:false,
-page:1
+  constructor() {
+    super();
+    console.log("Hello")
+    this.state = {
+      articles: this.articles,
+      loading: false,
+      page: 1
+    }
   }
-}
 
-async componentDidMount(){
-console.log("cmp")
-let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f9db77eef4174b9b8af7ca428ca9c97f&pageSize=${this.props.pageSize}&page=1`
-this.setState({loading:true})
-let data = await fetch(url);
-let paredData = await data.json()
-console.log(paredData)
-this.setState({articles:paredData.articles,
-  totalResults: paredData.totalResults,
-  loading:false})
-}
-hanlePrevClick = async () =>{
-  console.log("prev")
-  let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f9db77eef4174b9b8af7ca428ca9c97f&pageSize=${this.props.pageSize}&page=${this.state.page-1}`
-  this.setState({loading:true})
-  let data = await fetch(url);
-  let paredData = await data.json()
-  console.log(paredData)
-    this.setState({
-   page:this.state.page -1,
-   articles:paredData.articles,
-   loading:false
-    })
-}
-hanleNextClick = async () =>{
-  if(!(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize))){
-    console.log("next")
-    console.log(this.state.page)
-    console.log(this.state.totalResults)
-this.setState({loading:true})
-let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f9db77eef4174b9b8af7ca428ca9c97f&pageSize=${this.props.pageSize}&page=${this.state.page+1}`
-
-let data = await fetch(url);
-let paredData = await data.json()
+  async UpdateNews() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f9db77eef4174b9b8af7ca428ca9c97f&pageSize=${this.props.pageSize}&page=${this.state.page}`
+    this.setState({ loading: true })
+    let data = await fetch(url);
+    let paredData = await data.json()
     console.log(paredData)
     this.setState({
-      page:this.state.page +1,
-      articles:paredData.articles,
-      loading:false
+      articles: paredData.articles,
+      totalResults: paredData.totalResults,
+      loading: false
     })
   }
-}
+  async componentDidMount() {
+    this.UpdateNews()
+  }
+  hanlePrevClick = async () => {
+
+    this.setState({ page: this.state.page - 1 })
+    this.UpdateNews()
+  }
+  hanleNextClick = async () => {
+
+    this.setState({ page: this.state.page + 1 })
+    this.UpdateNews()
+  }
 
 
   render() {
@@ -62,20 +45,20 @@ let paredData = await data.json()
     return (
       <div className='container my-3'>
         <h1 className='text-center'>NewsPedia - Top Headlines</h1>
-        {this.state.loading && <Spinner/>}
+        {this.state.loading && <Spinner />}
         <div className="row">
-        {!this.state.loading && this.state.articles.map((elem)=>{
-          return  <div className="col-md-4 d-flex justify-content-center" key={elem.url}>
-            
-          <Newsitem  title={elem.title?elem.title:""} description={elem.description?elem.description:""} imageUrl={elem.urlToImage} newsUrl={elem.url}/>
-              </div>
-        })} 
+          {!this.state.loading && this.state.articles.map((elem) => {
+            return <div className="col-md-4 d-flex justify-content-center" key={elem.url}>
+
+              <Newsitem title={elem.title ? elem.title : ""} description={elem.description ? elem.description : ""} imageUrl={elem.urlToImage} newsUrl={elem.url} author={elem.author} date={elem.publishedAt} />
+            </div>
+          })}
         </div>
         <div className="container d-flex justify-content-center " >
-        <div className="btn-group" role="group" aria-label="Basic outlined example">
-  <button disabled={this.state.page<=1}type="button" className="btn btn-outline-primary" onClick={this.hanlePrevClick}>&larr; Previous</button>
-  <button disabled={this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-outline-primary" onClick={this.hanleNextClick}>Next &rarr;</button>
-</div>
+          <div className="btn-group" role="group" aria-label="Basic outlined example">
+            <button disabled={this.state.page <= 1} type="button" className="btn btn-outline-primary" onClick={this.hanlePrevClick}>&larr; Previous</button>
+            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-outline-primary" onClick={this.hanleNextClick}>Next &rarr;</button>
+          </div>
         </div>
       </div>
     )
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props
+    let { title, description, imageUrl, newsUrl, author, date } = this.props
     return (
       <div className='m-4 my-3 d-flex'>
         <div className="card">
-          <img src={imageUrl?imageUrl:"https://www.feednavigator.com/var/wrbm_gb_food_pharma/storage/images/9/2/8/5/235829-6-eng-GB/Feed-Test-SIC-Feed-20142.jpg"} className="card-img-top" alt="..." />
+          <img src={imageUrl ? imageUrl : "https://www.feednavigator.com/var/wrbm_gb_food_pharma/storage/images/9/2/8/5/235829-6-eng-GB/Feed-Test-SIC-Feed-20142.jpg"} className="card-img-top" alt="..." />
           <div className="card-body d-flex flex-column justify-content-between align-items-center p-3">
             <div>
-            <h5 className="card-title">{title?title:'No Title'}</h5>
-            <p className="card-text">{description?description:"No Description Available"}</p>
+              <h5 className="card-title">{title ? title : 'No Title'}</h5>
+              <p className="card-text">{description ? description : "No Description Available"}</p>
+              <p class="card-text"><small className="text-body-secondary">By {author ? author : "Unkown"} On {date ? new Date(date).toGMTString() : "Unknown Date"}</small></p>
             </div>
             <div>
-            <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+              <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
             </div>
           </div>
         </div>
